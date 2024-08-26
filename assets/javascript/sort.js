2679f50db76f1fd365d1d2821a293761fd1b7257
remy.ansar@gmail.com <remy.ansar@gmail.com>
2024-08-26 15:42:03 +0200

    fix(healthstatus) correction added to entity. The plantDetail is associated to the user and do not display health status from other users

--- assets/javascript/sort.js
+++ assets/javascript/sort.js
@@ -1,12 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.querySelector('form');
+// document.addEventListener('DOMContentLoaded', function() {
+//     const form = document.querySelector('form');
 
-    // Ajout d'un écouteur d'événement sur chaque select
-    const selects = document.querySelectorAll('select');
-    selects.forEach(select => {
-        select.addEventListener('change', function() {
-            form.submit();
-            console.log('test')
-        });
-    });
-});
+//     // Ajout d'un écouteur d'événement sur chaque select
+//     const selects = document.querySelectorAll('select');
+//     selects.forEach(select => {
+//         select.addEventListener('change', function() {
+//             form.submit();
+//             console.log('test')
+//         });
+//     });
+// });
