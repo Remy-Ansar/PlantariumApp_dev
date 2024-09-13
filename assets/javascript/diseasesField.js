ab91f46b61f7721737ed643692ef15d75f6b27c5
remy.ansar@gmail.com <remy.ansar@gmail.com>
2024-09-13 16:51:13 +0200

    fix(csslayout) better css response in local, to test in prod

--- assets/javascript/diseasesField.js
+++ assets/javascript/diseasesField.js
@@ -2,10 +2,17 @@ document.addEventListener('DOMContentLoaded', function() {
     const healthStatusSelect = document.getElementById('user_plant_detail_form_HealthStatus');
     const diseasesContainer = document.getElementById('diseases-container');
     
+    console.log('healthStatusSelect:', healthStatusSelect); // Vérifiez que cet élément est trouvé
+
+    if (!healthStatusSelect) {
+        console.error('Element with ID "user_plant_detail_form_HealthStatus" not found');
+        return; // Arrête l'exécution du script si l'élément n'existe pas
+    }
+
     // Fonction pour afficher ou masquer le champ des maladies
     function toggleDiseasesField() {
         // Vérifie si l'option "Malade" est sélectionnée
-        if (healthStatusSelect.value === '2') {
+        if (healthStatusSelect.value === '20') {
             diseasesContainer.classList.remove('hidden'); // Affiche le champ
         } else {
             diseasesContainer.classList.add('hidden'); // Masque le champ
